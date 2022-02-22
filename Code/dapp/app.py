0765010f08a3c35e9dcf1c43e1db3253502a49f7
Grep <mc13c@my.fsu.edu>
2022-02-21 21:23:11 -0500

    changes to dapp

--- Code/dapp/app.py
+++ Code/dapp/app.py
@@ -1,3 +1,5 @@
+# Imports
+##################################################################################
 from decimal import Decimal
 import os
 import json
@@ -6,21 +8,23 @@ from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
 from datetime import datetime
-# from bip44 import Wallet
 from web3.gas_strategies.time_based import medium_gas_price_strategy
 from web3.middleware import geth_poa_middleware
 
 
+# Initial Configurations
+#################################################################################
+
 load_dotenv()
 
 # Define and connect a new Web3 provider
-w3 = Web3(Web3.HTTPProvider('https://rinkeby.infura.io/v3/ae97deb9bbb14f0499e857aa136e10ad'))
+w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
+
+# Set streamlit page configuration
+st.set_page_config(page_title="CryptoBaras Minter", page_icon="🪙")
 
 
-# Set page configuration
-st.set_page_config(page_title="CryptoBaras Minter", page_icon="🆕")
 
-################################################################################
 # Contract Helper function:
 # 1. Loads the contract once using cache
 # 2. Connects to the contract using the contract address and ABI
@@ -36,7 +40,7 @@ def load_contract():
         contract_abi = json.load(f)
 
     # Set the contract address (this is the address of the deployed contract)
-    contract_address = "0xd843D6E8ce84f890Bb88F79b8aECE519e41657e5"
+    contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
 
     # Get the contract
     contract = w3.eth.contract(
@@ -51,18 +55,19 @@ def load_contract():
 contract = load_contract()
 
 
-################################################################################
-# Mint NFT
+# User-Friendly Interface
 ################################################################################
 
 # Set title
-st.markdown("# CRYPTOBARAS MINTER")
+original_title = '<p style="font-family:Fantasy; color:Blue; font-size: 20px;">CryptoBars Minter</p>'
+st.markdown(original_title, unsafe_allow_html=True)
 
 # Set page banner
 st.image('images/preview.gif')
 st.markdown("---")
 
 # Interact with contract
+##########################
 st.markdown("## There's only 100 CryptoBaras in Existence")
 
 st.markdown("Check how many are left for you to mint:")
@@ -79,30 +84,27 @@ if st.button("Cost"):
     st.write(cost)
 
 # Make a transaction
+###########################
+
 st.markdown("## Mint Your Own CryptoBara!")
 mintAmount = st.number_input("You Can Mint Up to 5 CryptoBaras", max_value=5, min_value=1)
-print(mintAmount, datetime.now())
 
 # Enter the purchaser's (To) wallet address
 minter_address = st.text_input("Enter Rinkeby Address")
-print("Minting to wallet address: " + minter_address, datetime.now())
 
 # Enter contract owner's wallet address
-contractowner_address = "0x24E8C8f8DA11aeD86b87fd72de1b3203233c50BD"
-contractowner_private_key = os.getenv("METAMASK_ACCOUNT2_PRIVATE_KEY")
+contractowner_address = os.getenv("CONTRACT_OWNER_ADDRESS")
+contractowner_private_key = os.getenv("CONTRACTOWNER_PRIVATE_KEY")
 
-# Build the transaction
 # Get Gas Estimate
 value = w3.toWei(0.02,'ether')
 w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
 
 # Get the nonce
 nonce = w3.eth.get_transaction_count(contractowner_address)
-print("Nonce:", nonce, datetime.now())
 
 # Middleware
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
-
 
 # Mint Button
 if st.button("Mint"):
@@ -110,6 +112,8 @@ if st.button("Mint"):
     value = value*mintAmount
     if minter_address == contractowner_address:
         value = 0
+
+    print(datetime.now(), "Amount: ", mintAmount, " Nonce:", nonce, " Minting to wallet: ", minter_address)
 
     transaction = {
         "from": contractowner_address,
@@ -119,15 +123,17 @@ if st.button("Mint"):
         "nonce": nonce
     }
 
-    print(datetime.now(), "line 122")
+    # Build the transaction
     transaction_build = contract.functions.mint(mintAmount, minter_address).buildTransaction(transaction)
-    print(datetime.now(), "line 123")
+    print(datetime.now(), "transaction built")
+
     signed_tx = w3.eth.account.sign_transaction(transaction_build, contractowner_private_key)
-    print(datetime.now(), "line 125")
+    print(datetime.now(), "transaction signed")
+
     txn_raw = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-    print(datetime.now(), "line 127")
+    print(datetime.now(), "raw transaction sent")
+
     txn_receipt = w3.eth.wait_for_transaction_receipt(txn_raw)
-    print(datetime.now(), "line 129")
     print(dict(txn_receipt))
 
     st.write("Transaction receipt mined:")
@@ -135,6 +141,7 @@ if st.button("Mint"):
 
 
 # Hide Streamlit Style
+#################################################################################
 hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
